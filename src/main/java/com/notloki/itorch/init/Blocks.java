3bd3236e18ad4104efdc8f7deb2c449d69d7ba61
not_loki <brianburke1030@live.com>
2017-06-24 12:06:57 -0500

    Code Cleanup

--- src/main/java/com/notloki/itorch/init/Blocks.java
+++ src/main/java/com/notloki/itorch/init/Blocks.java
@@ -6,22 +6,22 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(Ref.MODID)
 public class Blocks {
-        public static final Block I_TORCH = null;  // new BlockITorch("i_torch", "yellow");
-        public static final Block I_TORCH_BLUE = null; // new BlockITorch("i_torch_blue", "blue");
-        public static final Block I_TORCH_WHITE = null; // new BlockITorch("i_torch_white", "white");
-        public static final Block I_TORCH_GREEN = null; // new BlockITorch("i_torch_green", "green");
-        public static final Block I_TORCH_BLACK = null; // new BlockITorch("i_torch_black", "black");
-        public static final Block I_TORCH_RED = null; // new BlockITorch("i_torch_red", "red");
-        public static final Block I_TORCH_GRAY = null; // new BlockITorch("i_torch_gray", "gray");
-        public static final Block I_TORCH_LIME = null; // new BlockITorch("i_torch_lime", "lime");
-        public static final Block I_TORCH_CYAN = null; // new BlockITorch("i_torch_cyan", "cyan");
-        public static final Block I_TORCH_BROWN = null; // new BlockITorch("i_torch_brown", "brown");
-        public static final Block I_TORCH_PURPLE = null; // new BlockITorch("i_torch_purple", "purple");
-        public static final Block I_TORCH_PINK = null; // new BlockITorch("i_torch_pink", "pink");
-        public static final Block I_TORCH_ORANGE = null; // new BlockITorch("i_torch_orange", "orange");
-        public static final Block I_TORCH_MAGENTA = null; // new BlockITorch("i_torch_magenta", "magenta");
-        public static final Block I_TORCH_LIGHT_BLUE = null; // new BlockITorch("i_torch_light_blue", "light_blue");
-        public static final Block I_TORCH_LIGHT_GRAY = null; // new BlockITorch("i_torch_light_gray", "light_gray");
+        public static final Block I_TORCH = null;
+        public static final Block I_TORCH_BLUE = null;
+        public static final Block I_TORCH_WHITE = null;
+        public static final Block I_TORCH_GREEN = null;
+        public static final Block I_TORCH_BLACK = null;
+        public static final Block I_TORCH_RED = null;
+        public static final Block I_TORCH_GRAY = null;
+        public static final Block I_TORCH_LIME = null;
+        public static final Block I_TORCH_CYAN = null;
+        public static final Block I_TORCH_BROWN = null;
+        public static final Block I_TORCH_PURPLE = null;
+        public static final Block I_TORCH_PINK = null;
+        public static final Block I_TORCH_ORANGE = null;
+        public static final Block I_TORCH_MAGENTA = null;
+        public static final Block I_TORCH_LIGHT_BLUE = null;
+        public static final Block I_TORCH_LIGHT_GRAY = null;
 
     // public static final Block cavingFlare = new BlockCavingFlare("caving_flare");
 }
